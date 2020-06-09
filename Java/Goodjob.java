3accb5c60eaf244e2bd68b6bbebce96e4ef09ad1
tyagiashish74 <62237982+tyagiashish74@users.noreply.github.com>
2020-06-09 12:44:57 +0530

    commit

---
+++ Java/Goodjob.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.security.*;
+public class Solution {
+ public static void main(String[] args) {
+
+  DoNotTerminate.forbidExit();
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   //String s=???; Complete this line below
+
+   String s=String.valueOf(n);
+
+   
+   if (n == Integer.parseInt(s)) {
+    System.out.println("Good job");
+   } else {
+    System.out.println("Wrong answer.");
+   }
+  } catch (DoNotTerminate.ExitTrappedException e) {
+   System.out.println("Unsuccessful Termination!!");
+  }
+ }
+}
+
+//The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
+
+ public static class ExitTrappedException extends SecurityException {
+
+  private static final long serialVersionUID = 1;
+ }
+
+ public static void forbidExit() {
+  final SecurityManager securityManager = new SecurityManager() {
+   @Override
+   public void checkPermission(Permission permission) {
+    if (permission.getName().contains("exitVM")) {
+     throw new ExitTrappedException();
+    }
+   }
+  };
+  System.setSecurityManager(securityManager);
+ }
+}
+
+
+Or(My Solution.)
+
+
+import java.util.Scanner;
+
+public class hkrange {
+
+    public static void main(String []argh){
+        Scanner scan =  new Scanner(System.in);
+       int i = scan.nextInt();
+        if( i>=-100 && i<=100)
+            System.out.println("Good Job");
+        else
+        {
+            System.out.println("Wrong Answer.");
+        }
+
+    }
+}
