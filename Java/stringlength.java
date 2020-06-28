a04b5b5639e986e59a8259a24f944840763420d8
tyagiashish74 <62237982+tyagiashish74@users.noreply.github.com>
2020-06-28 11:21:53 +0530

    Add files via upload

---
+++ Java/stringlength.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+   public static void main(String[] args) {
+         
+         Scanner sc=new Scanner(System.in);
+         String A=sc.next();
+         String B=sc.next();
+         String output ="No";
+         /* Enter your code here. Print output to STDOUT. */
+         System.out.println(A.length()+B.length()); 
+
+	//here we compare A strin and B string. 
+         if(A.compareTo(B)>0) {
+          output ="Yes";
+         }
+         System.out.println(output);
+         System.out.println(A.substring(0, 1).toUpperCase()+""+A.substring(1)+" "+B.substring(0, 1).toUpperCase()+""+B.substring(1));
+     }
+}
+
+
+
